8eb582e901c1569bcd29ab3820c9abeaabb04d24
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-05-13 21:36:46 +0200

    Added level concept

--- game.js
+++ game.js
@@ -5,32 +5,86 @@ var toppos = 180;
 var height = 150;
 var vstep = 50;
 var messages = [
-    "Downgrading JDK",
-    "Ordering new ILO Licence",
     "Upgrading IDE",
     "Reinstalling GRUB",
-    "Restoring database backup"
+    "Restoring database backup",
+    "reading \"Java bytecode instruction listings\" on Wikipedia"
+];
+var levels = [
+    {
+        hair: 20,
+        time: 40000,
+        mission: "Upgrade server software",
+        steps: [
+            "Upgrade software",
+            "Reboot server",
+            "Scan network to find ILO address",
+            "Order new ILO Licence",
+            "Downgrade Java plugin",
+            "Reinstall GRUB"
+        ]
+    },
+    {
+        hair: 30,
+        time: 20000,
+        mission: "Fix simple software bug",
+        steps: [
+            "Upgrade IDE",
+            "Fix a lot of style guide warnings",
+            "Refacor code",
+            "Fix bug",
+            "Write test case",
+            "Fix build breakage caused by changes on the build server",
+        ]
+    }
+    
 ];
 
-function initialize() {
+function initializeLevel(level) {
     var game = $("#game");
-    for( var y = toppos; y < toppos + height; y += vstep) {
-	for( var x = left; x < left + width; x += hstep) {
-	    var xpos = left + width*Math.random();
-	    var ypos = y + 10*Math.random() - 5;
-	    var el = $('<div class="hair"/>').offset({top: ypos, left: xpos});
-	    game.append(el);
-	}
-    }
-    $('.hair').on('click', function() {
-	$("#messages").text(messages[Math.floor((Math.random()*messages.length))]);
-	var el = $(this);
-	el.hide();
-	window.setTimeout(function() {el.show();}, 20000);
-	if ($('.hair:visible').length === 0) {
-	    $("#game").hide();
-	}
+    var intro = $(".intro");
+    intro.detach();
+    intro.find('h2').text("Level " + (level + 1));
+    intro.find('strong').text("Mission: " + levels[level].mission);
+    intro.on('click', function() {
+        intro.hide();
     });
+    intro.show();
+    game.empty();
+    game.append(intro);
+    var hair = levels[level].hair;
+    var hedge = Math.sqrt(hair);
+    var vstep = height / hedge;
+    var hstep = width / hedge;
+    var time = levels[level].time;
+    hair = 0;
+    for( var y = toppos; y < toppos + height; y += vstep) {
+	    for( var x = left; x < left + width; x += hstep) {
+	        var xpos = left + width*Math.random();
+	        var ypos = y + 10*Math.random() - 5;
+	        var el = $('<div class="hair"/>').offset({top: ypos, left: xpos});
+	        game.append(el);
+            hair++;
+	    }
+    }
+    var steps = levels[level].steps;
+    $('.hair').on('click', function() {
+	    var el = $(this);
+	    el.hide();
+        var hairLeft = $('.hair:visible').length;
+        var step = steps.length - Math.floor((hairLeft/hair)*steps.length);
+        console.log("At step: " + step);
+	    $("#messages").text(steps[step]);
+	    window.setTimeout(function() {el.show();}, time);
+	    if (hairLeft === 0) {
+	        initializeLevel(level + 1);
+	    }
+    });
+}
+
+function initialize() {
+    var intro = $(".intro");
+    initializeLevel(0);
 }
 
 $(document).ready(initialize);
